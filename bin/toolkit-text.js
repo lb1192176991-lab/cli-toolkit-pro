ab18db4a126304f471c519aeba0b941addbfb0d1
Bobo <lb1192176991@gmail.com>
2026-05-25 01:11:13 +0800

    Add bin/toolkit-text.js

---
+++ bin/toolkit-text.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+try {
+  execSync(`text-processor-pro ${process.argv.slice(2).join(" ")}`, { stdio: "inherit" });
+} catch (e) {
+  process.exit(e.status || 1);
+}
