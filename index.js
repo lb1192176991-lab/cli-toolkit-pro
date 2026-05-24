6b3ea2a5235ee20cf8717f4f7f7348ff842bb189
Bobo <lb1192176991@gmail.com>
2026-05-25 01:10:59 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  csv: require("csv-toolkit-pro"),
+  json: require("json-tidy-pro"),
+  image: require("batch-image-tool"),
+  text: require("text-processor-pro"),
+  pdf: require("pdf-toolkit-pro"),
+  file: require("file-batch-toolkit"),
+};
